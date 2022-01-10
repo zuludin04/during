96ce22173c93db1f555af09d1840f45603c0bb8c
zuludin04 <zmauludin04@gmail.com>
2022-01-10 20:35:36 +0700

    filter icon in appbar

--- lib/ui/transaction/views/transaction_filter_screen.dart
+++ lib/ui/transaction/views/transaction_filter_screen.dart
@@ -1,4 +1,3 @@
-import 'package:during/core/utils/helper.dart';
 import 'package:during/core/widgets/toolbar_during.dart';
 import 'package:during/data/model/filter_transaction.dart';
 import 'package:during/ui/transaction/controllers/transaction_filter_controller.dart';
@@ -10,80 +9,37 @@ class TransactionFilterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: ToolbarDuring.defaultToolbar('Records'),
-      body: Column(
-        children: [
-          _filterBar(),
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return Text('Text #$index');
-              },
-              itemCount: 20,
+      appBar: ToolbarDuring.defaultToolbar(
+        'Records',
+        actions: [
+          IconButton(
+            onPressed: () async {
+              var result = await Get.bottomSheet(
+                FilterBottomSheet(),
+                isScrollControlled: true,
+                ignoreSafeArea: false,
+              );
+              if (result != null) {
+                if (result is FilterTransaction) {
+                  print('Filter range is ${result.range}');
+                }
+              } else {
+                var c = Get.find<TransactionFilterController>();
+                c.typed.value = c.filtered.type!;
+              }
+            },
+            icon: Icon(
+              Icons.filter_list,
+              color: Color(0xff373A36),
             ),
           ),
         ],
       ),
-    );
-  }
-
-  Widget _filterBar() {
-    return Container(
-      color: Colors.white,
-      padding: EdgeInsets.symmetric(vertical: 8),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Container(
-              height: kToolbarHeight / 2,
-              child: ListView.builder(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Container(
-                    width: 80,
-                    margin: EdgeInsets.symmetric(horizontal: 8),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      color: Color(0xffFFA400),
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: Text(
-                      dateRangeFilters[index],
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  );
-                },
-                itemCount: dateRangeFilters.length,
-              ),
-            ),
-          ),
-          SizedBox(
-            width: 50,
-            child: GestureDetector(
-              onTap: () async {
-                var result = await Get.bottomSheet(
-                  FilterBottomSheet(),
-                  isScrollControlled: true,
-                  ignoreSafeArea: false,
-                );
-                if (result != null) {
-                  if (result is FilterTransaction) {
-                    print('Filter range is ${result.range}');
-                  }
-                } else {
-                  var c = Get.find<TransactionFilterController>();
-                  c.typed.value = c.filtered.type!;
-                }
-              },
-              child: Icon(
-                Icons.filter_list,
-                color: Color(0xff373A36),
-              ),
-            ),
-          ),
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          return Text('Text #$index');
+        },
+        itemCount: 20,
       ),
     );
   }
