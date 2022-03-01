27a04c2951473ee81a2c6d8d8d062151c43b41fe
zuludin <zmauludin@bwa.id>
2022-03-01 08:23:11 +0700

    setup firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:during/core/utils/custom_theme.dart';
 import 'package:during/routes/app_pages.dart';
 import 'package:during/service/cache_service.dart';
 import 'package:during/service/repository_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
   await GetStorage.init();
+  
   runApp(MyApp());
 }
 
