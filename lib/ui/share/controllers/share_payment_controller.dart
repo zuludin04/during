83e90e724f36b5e2ce2573fd35cfca173254dd53
zuludin <zmauludin@bwa.id>
2022-03-12 11:11:05 +0700

    init sharepayment controller in screen

--- lib/ui/share/controllers/share_payment_controller.dart
+++ lib/ui/share/controllers/share_payment_controller.dart
@@ -3,6 +3,9 @@ import 'package:get/get.dart';
 class SharePaymentController extends GetxController {
   var generated = false.obs;
 
+  var paymentName = ''.obs;
+  var paymentNominal = '0'.obs;
+
   void generateQrCode() {
     generated.value = !generated.value;
   }
--- lib/ui/share/views/share_payment_screen.dart
+++ lib/ui/share/views/share_payment_screen.dart
@@ -1,10 +1,11 @@
 import 'package:during/core/widgets/input_text_field.dart';
 import 'package:during/core/widgets/toolbar_during.dart';
-import 'package:during/routes/app_pages.dart';
+import 'package:during/ui/share/controllers/share_payment_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SharePaymentScreen extends StatelessWidget {
+  final SharePaymentController _controller = Get.find();
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -21,18 +22,27 @@ class SharePaymentScreen extends StatelessWidget {
                 InputTextField(
                   title: 'Name',
                   hint: 'Name',
+                  onSaved: _controller.paymentName,
                 ),
                 const SizedBox(height: 16),
                 InputTextField(
                   title: 'Total Amount',
                   hint: 'Total Amount',
+                  keyboardType: TextInputType.number,
+                  currencyFormat: true,
+                  onSaved: _controller.paymentNominal,
                 ),
                 const SizedBox(height: 32),
                 ElevatedButton(
                   onPressed: () {
-                    Get.offNamed(RoutePath.GENERATED_CODE);
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                    }
                   },
-                  child: const Text('Generate Code'),
+                  child: const Text(
+                    'Create Payment',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
               ],
             ),
