e3e8986e50917bc8b97a58ef13cc738b7b23371d
zuludin <zmauludin@bwa.id>
2022-03-17 12:26:49 +0700

    language selected icon

--- lib/ui/language/language_change_screen.dart
+++ lib/ui/language/language_change_screen.dart
@@ -3,8 +3,26 @@ import 'package:during/service/cache_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class LanguageChangeScreen extends StatelessWidget {
+class LanguageChangeScreen extends StatefulWidget {
   const LanguageChangeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LanguageChangeScreen> createState() => _LanguageChangeScreenState();
+}
+
+class _LanguageChangeScreenState extends State<LanguageChangeScreen> {
+  late List<bool> _languageSelected;
+
+  @override
+  void initState() {
+    var locale = CacheService.to.selectedLanguage;
+    if (locale == 'en_US') {
+      _languageSelected = [true, false];
+    } else {
+      _languageSelected = [false, true];
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,16 +30,20 @@ class LanguageChangeScreen extends StatelessWidget {
       appBar: ToolbarDuring.defaultToolbar('language'.tr),
       body: ListView(
         children: [
-          ListTile(
-            title: const Text('English'),
-            onTap: () {
+          _languageItem(
+            'English',
+            _languageSelected[0],
+            () {
+              changeTypeState(0);
               CacheService.to.selectedLanguage = 'en_US';
               Get.updateLocale(const Locale('en', 'US'));
             },
           ),
-          ListTile(
-            title: const Text('Bahasa Indonesia'),
-            onTap: () {
+          _languageItem(
+            'Bahasa Indonesia',
+            _languageSelected[1],
+            () {
+              changeTypeState(1);
               CacheService.to.selectedLanguage = 'id_ID';
               Get.updateLocale(const Locale('id', 'ID'));
             },
@@ -29,5 +51,28 @@ class LanguageChangeScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Widget _languageItem(String title, bool selected, Function() onTap) {
+    return ListTile(
+      title: Text(title),
+      leading: Icon(
+        Icons.check,
+        color: selected ? Colors.black : Colors.transparent,
+      ),
+      onTap: onTap,
+    );
+  }
+
+  void changeTypeState(int index) {
+    setState(() {
+      for (int indexBtn = 0; indexBtn < _languageSelected.length; indexBtn++) {
+        if (indexBtn == index) {
+          _languageSelected[indexBtn] = true;
+        } else {
+          _languageSelected[indexBtn] = false;
+        }
+      }
+    });
   }
 }
