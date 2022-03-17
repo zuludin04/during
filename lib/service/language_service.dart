bb2ead8116c519380261cfc274f00174e8f51fcb
zuludin <zmauludin@bwa.id>
2022-03-17 10:40:44 +0700

    add transliteration

--- lib/service/language_service.dart
+++ lib/service/language_service.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 class LanguageService extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
-        'en_US': en_US,
-        'id_ID': id_ID,
+        'en_US': enUS,
+        'id_ID': idID,
       };
 }
