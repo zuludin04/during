96394c14e34c2eea3d2d0f41ef6463c1e54d184d
zuludin <zmauludin@bwa.id>
2022-05-29 15:35:18 +0700

    add ads configuration

---
+++ lib/core/utils/add_helper.dart
@@ -0,0 +1,10 @@
+class AdHelper {
+
+  static String get bannerAdUnitId {
+    return 'ca-app-pub-3940256099942544/6300978111';
+  }
+
+  static String get interstitialAdUnitId {
+    return "ca-app-pub-3940256099942544/1033173712";
+  }
+}
