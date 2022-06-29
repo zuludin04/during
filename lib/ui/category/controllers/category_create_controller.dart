3656af8a24b450ab6cf35c85ed17e890a33cdeb0
zuludin <zmauludin@bwa.id>
2022-06-29 07:02:15 +0700

    add new category

--- lib/ui/category/controllers/category_create_controller.dart
+++ lib/ui/category/controllers/category_create_controller.dart
@@ -1,3 +1,39 @@
+import 'package:during/data/during_repository.dart';
+import 'package:during/data/source/entity/category_entity.dart';
+import 'package:during/ui/category/controllers/category_dashboard_controller.dart';
 import 'package:get/get.dart';
 
-class CategoryCreateController extends GetxController {}
+class CategoryCreateController extends GetxController {
+  final DuringRepository _repository = Get.find();
+
+  var name = ''.obs;
+  var icon = 'icon_wallet.svg'.obs;
+  var type = 'Saving'.obs;
+
+  void insertCategory() async {
+    var category = CategoryEntity(
+      name: name.value,
+      icon: icon.value,
+      type: _typeToInt(type.value),
+    );
+
+    _repository.inserteCategroy(category).then((value) {
+      Get.find<CategoryDashboardController>()
+          .loadCategory(_typeToInt(type.value));
+      Get.back();
+    });
+  }
+
+  int _typeToInt(String type) {
+    switch (type) {
+      case 'Saving':
+        return 1;
+      case 'Income':
+        return 2;
+      case 'Expense':
+        return 3;
+      default:
+        return 1;
+    }
+  }
+}
--- lib/ui/category/views/category_create_screen.dart
+++ lib/ui/category/views/category_create_screen.dart
@@ -3,6 +3,7 @@ import 'package:during/core/widgets/header_text.dart';
 import 'package:during/core/widgets/input_text_field.dart';
 import 'package:during/core/widgets/toolbar_during.dart';
 import 'package:during/routes/app_pages.dart';
+import 'package:during/ui/category/controllers/category_create_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
@@ -15,8 +16,8 @@ class CategoryCreateScreen extends StatefulWidget {
 }
 
 class _CategoryCreateScreenState extends State<CategoryCreateScreen> {
-  String categoryType = 'Saving';
-  String categoryIcon = 'icon_wallet.svg';
+  final CategoryCreateController _controller = Get.find();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +26,12 @@ class _CategoryCreateScreenState extends State<CategoryCreateScreen> {
         'Category',
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                _formKey.currentState!.save();
+                _controller.insertCategory();
+              }
+            },
             icon: const Icon(
               Icons.check,
               color: Colors.black,
@@ -41,36 +47,39 @@ class _CategoryCreateScreenState extends State<CategoryCreateScreen> {
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Expanded(
-                  child: InputTextField(
-                    title: 'name'.tr,
-                    hint: 'name'.tr,
-                    // onSaved: _controller.name,
+                  child: Form(
+                    key: _formKey,
+                    child: InputTextField(
+                      title: 'name'.tr,
+                      hint: 'name'.tr,
+                      onSaved: _controller.name,
+                    ),
                   ),
                 ),
                 const SizedBox(width: 8),
-                InkWell(
-                  onTap: () async {
-                    var result = await Get.toNamed(RoutePath.categoryIcons);
-                    if (result != null) {
-                      setState(() {
-                        categoryIcon = result;
-                      });
-                    }
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xffffa400),
-                      borderRadius: BorderRadius.circular(5),
+                Obx(() {
+                  return InkWell(
+                    onTap: () async {
+                      var result = await Get.toNamed(RoutePath.categoryIcons);
+                      if (result != null) {
+                        _controller.icon.value = result;
+                      }
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xffffa400),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      padding: const EdgeInsets.all(9),
+                      child: SvgPicture.asset(
+                        'assets/category/${_controller.icon.value}',
+                        color: const Color(0xff373a36),
+                        width: 30,
+                        height: 30,
+                      ),
                     ),
-                    padding: const EdgeInsets.all(9),
-                    child: SvgPicture.asset(
-                      'assets/category/$categoryIcon',
-                      color: const Color(0xff373a36),
-                      width: 30,
-                      height: 30,
-                    ),
-                  ),
-                ),
+                  );
+                }),
               ],
             ),
             const SizedBox(height: 16),
@@ -82,22 +91,24 @@ class _CategoryCreateScreenState extends State<CategoryCreateScreen> {
                 borderRadius: BorderRadius.circular(5),
               ),
               child: DropdownButtonHideUnderline(
-                child: DropdownButton<String>(
-                  value: categoryType,
-                  items: types
-                      .map((e) => DropdownMenuItem<String>(
-                            value: e,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(e),
-                            ),
-                          ))
-                      .toList(),
-                  onChanged: (value) => setState(() {
-                    categoryType = value ?? "Saving";
-                  }),
-                  isExpanded: true,
-                ),
+                child: Obx(() {
+                  return DropdownButton<String>(
+                    value: _controller.type.value,
+                    items: types
+                        .map((e) => DropdownMenuItem<String>(
+                              value: e,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(e),
+                              ),
+                            ))
+                        .toList(),
+                    onChanged: (value) => setState(() {
+                      _controller.type.value = value ?? "Saving";
+                    }),
+                    isExpanded: true,
+                  );
+                }),
               ),
             ),
           ],
